629807ffe9c0f2443f5b29d82b20bf1e35c14b0e
Tash <tasm86688@gmail.com>
2026-07-26 16:16:09 +0300

    fix: format code using dart format

--- test/xmltv_parser_test.dart
+++ test/xmltv_parser_test.dart
@@ -1,10 +1,11 @@
 import 'package:test/test.dart';
+
 void main() {
   group('A group of tests', () {
     int value1 = 1;
     int value2 = 2;
 
-    int sum = value1+value2;
+    int sum = value1 + value2;
 
     test('First Test', () {
       expect(sum, 3);
